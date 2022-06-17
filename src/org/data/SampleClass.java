fc2491090a95e39df5e30c900ea05f07057c90b2
ajai133 <ajai133@gmail.com>
2022-06-17 17:21:51 +0530

    This is Maven Pro Project

---
+++ src/org/data/SampleClass.java
@@ -0,0 +1,24 @@
+package org.data;
+
+public class SampleClass {
+
+	private void tc1() {
+		
+		System.out.println("tc1");
+
+	}
+
+	private void tc2() {
+		
+		System.out.println("tc2");
+
+	}
+	public static void main(String[] args) {
+		
+		SampleClass s = new SampleClass();
+		
+		s.tc1();
+		
+		s.tc2();
+	}
+}
